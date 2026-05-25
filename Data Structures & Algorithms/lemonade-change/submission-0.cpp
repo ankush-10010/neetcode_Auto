598944041662a4b2c4c429d45c67aecaef0b21d0
Ankush Singh <ankushraj2024@gmail.com>
2026-05-25 13:39:44 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0;
+        int tens = 0;
+        
+        for (int bill : bills) {
+            if (bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                fives--;
+                tens++;
+            } else if (bill == 20) {
+                if (tens > 0 && fives > 0) {
+                    tens--;
+                    fives--;
+                } else {
+                    fives -= 3;
+                }
+            }
+            
+            if (fives < 0) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
