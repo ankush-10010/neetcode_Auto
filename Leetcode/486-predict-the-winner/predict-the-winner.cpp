bbad442e06280ae160e38d3a59a39471ed0e8b29
Ankush Raj <ankushraj2024@gmail.com>
2026-08-02 08:35:57 +0530

    Time: 2 ms (43.99%) | Memory: 10.3 MB (16.32%) - LeetSync

--- Leetcode/486-predict-the-winner/predict-the-winner.cpp
+++ Leetcode/486-predict-the-winner/predict-the-winner.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
-    bool solve(int i,int j,int scoreA,int scoreB,bool aturn,vector<int>& nums){
-        if(i>j){
-            return scoreA >= scoreB;
-        }
-        if(aturn){
-            //updation in scoreA
-            // scoreA += nums[i];
-            aturn = !aturn;
-            bool left = solve(i+1,j,scoreA+nums[i],scoreB,aturn,nums);
-            bool right = solve(i,j-1,scoreA+nums[j],scoreB,aturn,nums);
-            aturn = !aturn;
-            return left || right;
-        }else{
-            //updation in scoraB
-            aturn = !aturn;
-            bool left = solve(i+1,j,scoreA,scoreB+nums[i],aturn,nums);
-            bool right = solve(i,j-1,scoreA,scoreB+nums[j],aturn,nums);
-            aturn = !aturn;
-            return left && right;
-        }
+    int solve(int i,int j,vector<int>& nums,vector<vector<int>>& memo){
+        if(i>j) return 0;
+        if(memo[i][j] != -1) return memo[i][j];
+        int left = nums[i] - solve(i+1,j,nums,memo); 
+        int right = nums[j] - solve(i,j-1,nums,memo);
+        return memo[i][j] = max(left,right); 
     }
     bool predictTheWinner(vector<int>& nums) {
-        return solve(0,nums.size()-1,0,0,true,nums);
+        vector<vector<int>> memo(nums.size(),vector<int>(nums.size(),-1));
+        return solve(0,nums.size()-1,nums,memo)>=0;
     }
 };
