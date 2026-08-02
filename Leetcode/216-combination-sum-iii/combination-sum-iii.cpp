5d7fc7764c9b2a879bab91a1baa684c556b631e9
Ankush Raj <ankushraj2024@gmail.com>
2026-08-02 16:07:28 +0530

    Time: 7 ms (5.20%) | Memory: 18.6 MB (5.44%) - LeetSync

--- Leetcode/216-combination-sum-iii/combination-sum-iii.cpp
+++ Leetcode/216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,24 +1,49 @@
 class Solution {
+    using vvi = vector<vector<int>>;
 public:
-    void backtrack(int k,int n,int start,int currSum,vector<int>& current,vector<vector<int>>& result){
-        if(currSum > n){
-            return;
+    // void backtrack(int k,int n,int start,int currSum,vector<int>& current,vector<vector<int>>& result){
+    //     if(currSum > n){
+    //         return;
+    //     }
+    //     if(current.size() == k){
+    //         if(currSum == n){
+    //             result.push_back(current);
+    //         }
+    //     }
+    //     for(int i=start;i<=9;i++){
+    //         current.push_back(i);
+    //         backtrack(k,n,i+1,currSum+i,current,result);
+    //         current.pop_back();
+    //     }
+    // }
+    vector<vector<int>> backtrack(int k_left,int n_left,int start,vector<vector<vector<vvi>>>& memo,vector<vector<vector<bool>>>& visited){
+        if(k_left == 0 && n_left == 0) return {{}};
+        if(k_left == 0 || n_left < 0 || start > 9) return {};
+
+        if(visited[start][k_left][n_left]) return memo[start][k_left][n_left];
+
+        vvi current_combinations;
+
+        vvi skip = backtrack(k_left,n_left,start+1,memo,visited);
+        for(auto& comb:skip){
+            current_combinations.push_back(comb);
         }
-        if(current.size() == k){
-            if(currSum == n){
-                result.push_back(current);
-            }
+        vvi take = backtrack(k_left-1,n_left-start,start+1,memo,visited);
+        for(auto& comb:take){
+            comb.push_back(start);
+            current_combinations.push_back(comb);
         }
-        for(int i=start;i<=9;i++){
-            current.push_back(i);
-            backtrack(k,n,i+1,currSum+i,current,result);
-            current.pop_back();
-        }
+        visited[start][k_left][n_left] = true;
+        return memo[start][k_left][n_left] = current_combinations;
+
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<vector<int>>result;
-        vector<int>current;
-        backtrack(k,n,1,0,current,result);
-        return result;
+        // vector<vector<int>>result;
+        // vector<int>current;
+        // backtrack(k,n,1,0,current,result);
+        // return result;
+        vector<vector<vector<bool>>> visited(11,vector<vector<bool>>(11,vector<bool>(65,false)));
+        vector<vector<vector<vvi>>> memo(11,vector<vector<vvi>>(11,vector<vvi>(65)));
+        return backtrack(k,n,1,memo,visited);
     }
 };
