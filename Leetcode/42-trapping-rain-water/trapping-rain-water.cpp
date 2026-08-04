564e7275d06ec396493f7fba45a5bbf258ce1172
Ankush Raj <ankushraj2024@gmail.com>
2026-08-04 16:24:30 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (93.89%) - LeetSync

--- Leetcode/42-trapping-rain-water/trapping-rain-water.cpp
+++ Leetcode/42-trapping-rain-water/trapping-rain-water.cpp
@@ -12,18 +12,12 @@ public:
 
         while(left < right){
             if(height[left] < height[right]){
-                if(height[left] >= left_max){
-                    left_max = height[left];
-                }else{
-                    total_water += left_max - height[left];
-                }
+                left_max = max(left_max,height[left]);
+                total_water += left_max - height[left];
                 left++;
             }else{
-                if(height[right] >= right_max){
-                    right_max = height[right];
-                }else{
-                    total_water += right_max - height[right];
-                }
+                right_max = max(right_max,height[right]);
+                total_water += right_max - height[right];
                 right--;
             }
         }
