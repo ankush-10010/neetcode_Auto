252868c248dae52ae7fc3895913e3701a8a743f0
Ankush Raj <ankushraj2024@gmail.com>
2026-08-12 14:58:58 +0530

    Time: 91 ms (43.58%) | Memory: 14 MB (76.30%) - LeetSync

---
+++ Leetcode/869-similar-string-groups/similar-string-groups.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<int> parent;
+    int groupCount;
+    int findParent(int x){
+        if(parent[x] == x) return x;
+        parent[x] = findParent(parent[x]);
+        return parent[x];
+    }
+    void union1(int x, int y){
+        int leaderX = findParent(x);
+        int leaderY = findParent(y);
+        if(leaderX != leaderY){
+            parent[leaderX] = leaderY;
+            groupCount--;
+        }
+    }
+    bool isSimilar(const string& s1,const string& s2){
+        int mismatches = 0;
+        for(int i=0;i<s1.size();i++){
+            if(s1[i] != s2[i]) mismatches++;
+        }
+        return mismatches == 0 || mismatches == 2;
+    }
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+        parent.resize(n);
+        groupCount = n;
+
+        for(int i=0;i<n;i++){
+            parent[i] = i;
+        } 
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(isSimilar(strs[i],strs[j])){
+                    union1(i,j);
+                }
+            }
+        }
+        return groupCount;
+    }
+};
