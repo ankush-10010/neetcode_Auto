abeb1f0662f01bf94eb6466b6cc12d857284caf9
Ankush Raj <ankushraj2024@gmail.com>
2026-08-14 11:21:47 +0530

    Time: 7 ms (22.37%) | Memory: 18.3 MB (13.46%) - LeetSync

--- Leetcode/740-delete-and-earn/delete-and-earn.cpp
+++ Leetcode/740-delete-and-earn/delete-and-earn.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
-    int sovle(unordered_map<int,int>& mp,int num,int &maxPoint,int &currPoint){
-        mp[num]--;
-        int c_n1 = mp[num-1];
-        int c_n2 = mp[num+1];
-        mp[num-1] = 0;
-        mp[num+1] = 0;
-        currPoint += num;
-        maxPoint = max(maxPoint,currPoint);
-        return 0;
+    int solve(int i,vector<int>& memo,unordered_map<int,int>& mp){
+        if(i==0) return 0;
+        if(i==1) return max(mp[1]*1,mp[0]*0);
+        if(memo[i] != -1) return memo[i];
+        int skip = solve(i-1,memo,mp);
+        int take = mp[i]*i + solve(i-2,memo,mp);
+        return memo[i] = max(skip,take);
     }
     int deleteAndEarn(vector<int>& nums) {
         if(nums.size() == 0) return 0;
@@ -18,17 +16,8 @@ public:
             freq[num]++;
             max_num = max(max_num,num);
         }
-        // for(auto& [num,freq] : mp){
-        //     solve(mp,num);
-        // }
-        vector<int> dp(max_num+1,0);
-        dp[0] = (freq[0] > 0) ? 0*freq[0] : 0;
-        dp[1] = 1*freq[1];
-
-        for(int i=2;i<max_num+1;i++){
-            dp[i] = max(dp[i-1],dp[i-2] + freq[i]*i);
-        }
-
-        return dp[max_num];
+        
+        vector<int> memo(max_num+1,-1);
+        return solve(max_num,memo,freq);
     }
 };
