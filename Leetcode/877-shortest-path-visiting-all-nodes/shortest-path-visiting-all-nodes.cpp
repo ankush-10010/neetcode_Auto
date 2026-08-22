b51f3a6738b5a678de81de2a5ffee711999b21b4
Ankush Raj <ankushraj2024@gmail.com>
2026-08-22 11:45:56 +0530

    Time: 14 ms (48.49%) | Memory: 15.3 MB (62.13%) - LeetSync

--- Leetcode/877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
+++ Leetcode/877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.cpp
@@ -3,36 +3,30 @@ public:
     int shortestPathLength(vector<vector<int>>& graph) {
         int n = graph.size();
         if(n == 1) return 0;
-        queue<pair<int,pair<int,int>>> q;
+        queue<tuple<int,int,int>> q;
         vector<vector<bool>> visited(n,vector<bool>(1<<n,false));
 
         for(int i=0;i<n;i++){
             int currMask = (1<<i);
-            q.push({i,{currMask,0}});
+            q.push({i,currMask,0});
             visited[i][currMask] = true;
         }
 
-        int completedMask = (1<<n) - 1;
+        int lastState = (1<<n) - 1;
         while(!q.empty()){
-            auto top = q.front();
+            auto [currNode,currMask,currDist] = q.front();
             q.pop();
-            int Currnode = top.first;
-            int CurrMask = top.second.first;
-            int CurrDist = top.second.second;
 
-            if(CurrMask == completedMask){
-                return CurrDist;
-            }
+            if(currMask == lastState) return currDist;
 
-            for(int neigh : graph[Currnode]){
-                if(!visited[neigh][CurrMask]){
-                    visited[neigh][CurrMask] = true;
-                    int neighMask = CurrMask | (1<<neigh);
-                    q.push({neigh,{neighMask,CurrDist+1}});
+            for(int neigh : graph[currNode]){
+                if(!visited[neigh][currMask]){
+                    visited[neigh][currMask] = true;
+                    int neighMask = currMask | (1<<neigh);
+                    q.push({neigh,neighMask,currDist+1});
                 }
             }
         }
-
         return 0;
     }
 };
