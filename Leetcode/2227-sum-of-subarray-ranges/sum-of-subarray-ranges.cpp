72a5e31e1474baa7f91142d6a6b7dd427148f32e
Ankush Raj <ankushraj2024@gmail.com>
2026-08-25 19:45:29 +0530

    Time: 5 ms (49.65%) | Memory: 15.7 MB (54.60%) - LeetSync

--- Leetcode/2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
+++ Leetcode/2227-sum-of-subarray-ranges/sum-of-subarray-ranges.cpp
@@ -2,18 +2,69 @@ class Solution {
 public:
     long long subArrayRanges(vector<int>& nums) {
         int n = nums.size();
+        vector<int> minPrev(n,-1);
+        vector<int> minNext(n,n);
+        vector<int> maxPrev(n,-1);
+        vector<int> maxNext(n,n);
+
+        stack<int> st;
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()] > nums[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                minPrev[i] = st.top();
+            }
+            st.push(i);
+        }
+        while(!st.empty()) st.pop();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()] >= nums[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                minNext[i] = st.top();
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+
+        for(int i=0;i<n;i++){
+            while(!st.empty() && nums[st.top()] < nums[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                maxPrev[i] = st.top();
+            }
+            st.push(i);
+        }
+
+        while(!st.empty()) st.pop();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && nums[st.top()] <= nums[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                maxNext[i] = st.top();
+            }
+            st.push(i);
+        }
+
         long long total = 0;
         for(int i=0;i<n;i++){
-            int minE = INT_MAX;
-            int maxE = INT_MIN;
-            
-            for(int j=i;j<n;j++){
-                long long currentCount = 0;
-                minE = min(minE,nums[j]);
-                maxE = max(maxE,nums[j]);
-                currentCount += (maxE - minE);
-                total += currentCount;
-            }
+            long long leftMinChoices = i - minPrev[i];
+            long long rightMinChoices = minNext[i] - i;
+            long long minSubarrays = rightMinChoices*leftMinChoices;
+
+            long long leftMaxChoices = i - maxPrev[i];
+            long long rightMaxChoices = maxNext[i] - i;
+            long long maxSubarrays = rightMaxChoices*leftMaxChoices;
+
+            total += (maxSubarrays * nums[i]) - (minSubarrays * nums[i]);
         }
 
         return total;
