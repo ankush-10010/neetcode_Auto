409e4b60acf80da1a542be0e1d4c08edbf6fb53a
Ankush Raj <ankushraj2024@gmail.com>
2026-08-25 19:18:52 +0530

    Time: 68 ms (69.14%) | Memory: 193.4 MB (74.99%) - LeetSync

--- Leetcode/943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
+++ Leetcode/943-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -1,24 +1,41 @@
 class Solution {
 public:
     int sumSubarrayMins(vector<int>& arr) {
+        int MOD = 1e9+7;
         int n = arr.size();
-        long long MOD = 1e9 + 7;
-        stack<int> st;
-        long long totalCount = 0;
-        for(int i=0;i<=n;i++){
-            while(!st.empty() && (i==n || arr[st.top()] >= arr[i])){
-                int mid = st.top();
-                st.pop();
+        vector<int> leftBound(n, -1); 
+        stack<int> st_left;
 
-                int leftBound = st.empty() ? -1 : st.top();
-                int rightBound = i;
-
-                long long subArrays_sum = (1LL*(mid-leftBound)*(rightBound-mid)) % MOD;
-                totalCount = (totalCount + (subArrays_sum * arr[mid]) % MOD) % MOD;
+        for (int i = 0; i < n; i++) {
+            while (!st_left.empty() && arr[st_left.top()] > arr[i]) {
+                st_left.pop();
             }
-            st.push(i);
+            if (!st_left.empty()) {
+                leftBound[i] = st_left.top();
+            }
+            st_left.push(i);
         }
 
-        return totalCount;
+        vector<int> rightBound(n,n);
+        stack<int> st_right;
+
+        for(int i=n-1;i>=0;i--){
+            while(!st_right.empty() && arr[st_right.top()] >= arr[i]){
+                st_right.pop();
+            }
+            if(!st_right.empty()){
+                rightBound[i] = st_right.top();
+            }
+            st_right.push(i);
+        }
+
+        long long total = 0;
+        for(int i=0;i<n;i++){
+            long long left = (i-leftBound[i])%MOD;
+            long long right = (rightBound[i]-i)%MOD;
+            long long current = arr[i];
+            total = (total + (left*right*current)%MOD)%MOD;
+        }
+        return total;
     }
 };
