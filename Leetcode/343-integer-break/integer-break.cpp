e0d3d4eff706ea51a30a0fd9a34613f18f53466a
Ankush Raj <ankushraj2024@gmail.com>
2026-08-28 15:51:15 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (63.62%) - LeetSync

--- Leetcode/343-integer-break/integer-break.cpp
+++ Leetcode/343-integer-break/integer-break.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        vector<int> dp(n+1,1);
-        dp[0] = 0;
-        for(int i=2;i<=n;i++){
-            cout << "debug for i --> " << i << endl;
-            int maxTillnow = 1;
-            for(int j=0;j<i;j++){
-                // cout << "debug for i --> "<< i << "debug for j--> " << j<< endl;
-                // cout << format("i-j-->{}  dp[j]-->{}",i-j,dp[j])<<endl;
-                // cout << format("dp[i-j]-->{}  dp[j]-->{}",i-j,j)<<endl;
-                // cout << format("i-j -->{}  j-->{}",i-j,j)<<endl;
-                maxTillnow = max({maxTillnow,(i-j)*dp[j],dp[i-j]*dp[j],(i-j)*j});
-                // cout << format("maxTillnow --> {}",maxTillnow);
-            }
-            dp[i] = max(dp[i],maxTillnow);
-            // cout << format("dp[i] --> {}" , dp[i]);
+        if(n==2) return 1;
+        if(n==3) return 2;
+
+        int q = n/3;
+        int rem = n%3;
+
+        if(rem == 0){
+            return pow(3,q);
         }
-        return dp[n];
+        if(rem == 1) return pow(3,q-1)*4;
+        if(rem == 2) return pow(3,q)*2;
+
+        return 0;
     }
 };
